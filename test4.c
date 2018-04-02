41e55aac8ebf26508a09d20b5718da8877b50ea1
Eva Arevalo <93.evar@gmail.com>
2018-04-02 23:52:09 +0800

    Added testcas4.c

---
+++ test4.c
@@ -0,0 +1,22 @@
+b = 12 + 12.3E-ss +24; 
+b = 12 + 12.3E-24;
+b = 12 + 12.3E-+24; 
+452
+ad2
+45ab2
+10.56
+10.56.23
+12.3e2
+12e+11
+12.3E+asb2
+"abcdefg"
+“abcdefg
+_id
+@error
+#pragma something off
+/*multilne comment
+second line
+haven't ended
+even more comments
+continue comment
+finally finished*/
